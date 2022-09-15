d2c9c750ce15e3b3011f94fe7df15c4af41bdd84
Hamza <https://github.com/ilhan2021/PostgreSQL.git>
2022-09-16 00:32:58 +0300

    JavaA

--- src/com/tpe/generics/GenericType.java
+++ src/com/tpe/generics/GenericType.java
@@ -1,41 +1,40 @@
 package com.tpe.generics;
 
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class GenericType <T> {
-	private T typeT;
 	
+	private T type;
 	
+//generic lerde aşağıdaki gibi static kullanılmıyor
+//	public static T getType() {
+//		return type;
+//	}
 	
-	
-	
-	
-	public T getTypeT() {
-		return typeT;
-	}
-
-	public void setTypeT(T typeT) {
-		this.typeT = typeT;
+	public  T getType() {
+		return type;
 	}
 
 
+	public void setType(T type) {
+		this.type = type;
+	}
 
 	public static void main(String[] args) {
-	
-		GenericType<String> obj1 = new GenericType<>();
-		obj1.setTypeT("Generic Types in Java");
+		GenericType<String> obj1=new GenericType<>();
+		obj1.setType("Generic Types in Java");
 		
-		GenericType<Integer> obj2 = new GenericType<>();
-		obj2.setTypeT(65);
+		GenericType<Integer> obj2=new GenericType<>();
+		obj2.setType(65);
 		
-		List<String> list = new ArrayList<>();
+		
+		List<String> list=new ArrayList<>();
 		list.add("Java");
 		list.add("Generic");
-		//list.add(new Point());
+		//list.add(new Point()); Compile Time Error
 		
 		Map<Integer,String> map=new HashMap<>();
 		map.put(1, "Java");
@@ -44,7 +43,6 @@ public class GenericType <T> {
 		
 		
 		
-		
-}
 
+	}
 }
--- src/com/tpe/generics/GenericTypeTwoParam.java
+++ src/com/tpe/generics/GenericTypeTwoParam.java
@@ -8,52 +8,35 @@ public class GenericTypeTwoParam <S,U> {
 	private U u;
 	
 	public GenericTypeTwoParam(S s, U u) {
-		super();
 		this.s = s;
 		this.u = u;
 	}
-
-
 
 	public S getS() {
 		return s;
 	}
 
-
 	public void setS(S s) {
 		this.s = s;
 	}
-
-
-
-
-
 
 	public U getU() {
 		return u;
 	}
 
 
-
-
-
-
 	public void setU(U u) {
 		this.u = u;
 	}
-
-
-
-
-
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		GenericTypeTwoParam<String, Integer> gt = new GenericTypeTwoParam("Java", 1);
-		GenericTypeTwoParam<Integer, Point>  gt2 = new GenericTypeTwoParam(1,new Point(10, 50));
+		GenericTypeTwoParam<String, Integer> gt=new GenericTypeTwoParam<>("Java", 1);
+		GenericTypeTwoParam<Integer,Point> gt2=new GenericTypeTwoParam<>(1, new Point(10, 50));
 		
 		System.out.println(gt.getS());
 		System.out.println(gt.getU());
+
 	}
 
 }
